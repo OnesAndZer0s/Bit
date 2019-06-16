14e32709e878ff9613b18ad1c802f9e114d99b38
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2019-06-16 12:49:36 -0500

    Update css_blocks.js

--- css/css_blocks.js
+++ css/css_blocks.js
@@ -8,10 +8,271 @@ Blockly.Blocks['css_block'] = {
         .appendField("declarations");
     this.appendStatementInput("declarations")
         .setCheck("declaration");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
+    this.setPreviousStatement(true, "css");
+    this.setNextStatement(true, "css");
     this.setColour(45);
  this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_class_selector'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("class")
+        .appendField(new Blockly.FieldTextInput("class name"), "className");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_id_selector'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("id")
+        .appendField(new Blockly.FieldTextInput("id name"), "idName");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_join_selectors'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("join");
+    this.appendStatementInput("element")
+        .setCheck("selector");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_descendant_selectors'] = {
+  init: function() {
+    this.appendStatementInput("element1")
+        .setCheck("selector");
+    this.appendDummyInput()
+        .appendField("in");
+    this.appendStatementInput("element2")
+        .setCheck("selector");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_allelement_selectors'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("all elements");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_element_selectors'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("element")
+        .appendField(new Blockly.FieldDropdown([["option","OPTIONNAME"], ["option","OPTIONNAME"], ["option","OPTIONNAME"]]), "elementDrop");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_parent_selectors'] = {
+  init: function() {
+    this.appendStatementInput("element1")
+        .setCheck("selector");
+    this.appendDummyInput()
+        .appendField("with parent");
+    this.appendStatementInput("element2")
+        .setCheck("selector");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_after_selectors'] = {
+  init: function() {
+    this.appendStatementInput("element1")
+        .setCheck("selector");
+    this.appendDummyInput()
+        .appendField("immediately after");
+    this.appendStatementInput("element2")
+        .setCheck("selector");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_preceded_selectors'] = {
+  init: function() {
+    this.appendStatementInput("element1")
+        .setCheck("selector");
+    this.appendDummyInput()
+        .appendField("preceded by");
+    this.appendStatementInput("element2")
+        .setCheck("selector");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_not_selectors'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("not");
+    this.appendStatementInput("notElement")
+        .setCheck("selector");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_attribute_selectors'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("attribute")
+        .appendField(new Blockly.FieldTextInput("attribute name"), "attributeName");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_attribute_with_selectors'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("attribute")
+        .appendField(new Blockly.FieldTextInput("attribute name"), "attributeName")
+        .appendField("with")
+        .appendField(new Blockly.FieldTextInput("attribute value"), "attributeValue");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_attribute_containing_selectors'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("attribute")
+        .appendField(new Blockly.FieldTextInput("attribute name"), "attributeName")
+        .appendField("containing")
+        .appendField(new Blockly.FieldTextInput("attribute value"), "attributeValue");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_attribute_starting_selectors'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("attribute")
+        .appendField(new Blockly.FieldTextInput("attribute name"), "attributeName")
+        .appendField("starting")
+        .appendField(new Blockly.FieldTextInput("attribute value"), "attributeValue");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_attribute_begins_selectors'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("attribute")
+        .appendField(new Blockly.FieldTextInput("attribute name"), "attributeName")
+        .appendField("begins")
+        .appendField(new Blockly.FieldTextInput("attribute value"), "attributeValue");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_attribute_ends_selectors'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("attribute")
+        .appendField(new Blockly.FieldTextInput("attribute name"), "attributeName")
+        .appendField("ends")
+        .appendField(new Blockly.FieldTextInput("attribute value"), "attributeValue");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_attribute_substring_selectors'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("attribute")
+        .appendField(new Blockly.FieldTextInput("attribute name"), "attributeName")
+        .appendField("substring")
+        .appendField(new Blockly.FieldTextInput("attribute value"), "attributeValue");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['css_pseudo_selectors'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("pseudo")
+        .appendField(new Blockly.FieldDropdown([["option","OPTIONNAME"], ["option","OPTIONNAME"], ["option","OPTIONNAME"]]), "pseudoDrop");
+    this.appendStatementInput("pseudo")
+        .setCheck("selector");
+    this.setPreviousStatement(true, "selector");
+    this.setNextStatement(true, "selector");
+    this.setColour(345);
+ this.setTooltip("https://www.w3schools.com/css/css_pseudo_classes.asp");
  this.setHelpUrl("");
   }
 };
@@ -34,3 +295,139 @@ return "console.log('YEET');";
 //https://developers.google.com/blockly/guides/create-custom-blocks/define-blocks#label
 };
 
+
+Blockly.JavaScript['css_class_selector'] = function(block) {
+  var text_classname = block.getFieldValue('className');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_id_selector'] = function(block) {
+  var text_idname = block.getFieldValue('idName');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_join_selectors'] = function(block) {
+  var statements_element = Blockly.JavaScript.statementToCode(block, 'element');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_descendant_selectors'] = function(block) {
+  var statements_element1 = Blockly.JavaScript.statementToCode(block, 'element1');
+  var statements_element2 = Blockly.JavaScript.statementToCode(block, 'element2');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_allelement_selectors'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_element_selectors'] = function(block) {
+  var dropdown_elementdrop = block.getFieldValue('elementDrop');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_parent_selectors'] = function(block) {
+  var statements_element1 = Blockly.JavaScript.statementToCode(block, 'element1');
+  var statements_element2 = Blockly.JavaScript.statementToCode(block, 'element2');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_after_selectors'] = function(block) {
+  var statements_element1 = Blockly.JavaScript.statementToCode(block, 'element1');
+  var statements_element2 = Blockly.JavaScript.statementToCode(block, 'element2');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_preceded_selectors'] = function(block) {
+  var statements_element1 = Blockly.JavaScript.statementToCode(block, 'element1');
+  var statements_element2 = Blockly.JavaScript.statementToCode(block, 'element2');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_not_selectors'] = function(block) {
+  var statements_notelement = Blockly.JavaScript.statementToCode(block, 'notElement');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_attribute_selectors'] = function(block) {
+  var text_attributename = block.getFieldValue('attributeName');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_attribute_with_selectors'] = function(block) {
+  var text_attributename = block.getFieldValue('attributeName');
+  var text_attributevalue = block.getFieldValue('attributeValue');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_attribute_containing_selectors'] = function(block) {
+  var text_attributename = block.getFieldValue('attributeName');
+  var text_attributevalue = block.getFieldValue('attributeValue');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_attribute_starting_selectors'] = function(block) {
+  var text_attributename = block.getFieldValue('attributeName');
+  var text_attributevalue = block.getFieldValue('attributeValue');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_attribute_begins_selectors'] = function(block) {
+  var text_attributename = block.getFieldValue('attributeName');
+  var text_attributevalue = block.getFieldValue('attributeValue');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_attribute_ends_selectors'] = function(block) {
+  var text_attributename = block.getFieldValue('attributeName');
+  var text_attributevalue = block.getFieldValue('attributeValue');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_attribute_substring_selectors'] = function(block) {
+  var text_attributename = block.getFieldValue('attributeName');
+  var text_attributevalue = block.getFieldValue('attributeValue');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['css_pseudo_selectors'] = function(block) {
+  var dropdown_pseudodrop = block.getFieldValue('pseudoDrop');
+  var statements_pseudo = Blockly.JavaScript.statementToCode(block, 'pseudo');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
