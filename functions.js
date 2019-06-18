74e5672a27c418eb4c8b279bb73be498b2fafbd1
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2019-06-18 09:24:08 -0500

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,29 @@
+function initBlockly(div,options,updateFunc){
+  var blocklyArea = document.getElementById(div);
+  var blocklyDiv = document.createElement('div');
+  blocklyDiv.style.position = "absolute";
+  blocklyArea.appendChild(blocklyDiv);
+  var workspace = Blockly.inject(blocklyDiv,options);
+  var onresize = function(e) {
+    // Compute the absolute coordinates and dimensions of blocklyArea.
+    var element = blocklyArea;
+    var x = 0;
+    var y = 0;
+    do {
+      x += element.offsetLeft;
+      y += element.offsetTop;
+      element = element.offsetParent;
+    } while (element);
+    // Position blocklyDiv over blocklyArea.
+    blocklyDiv.style.left = x + 'px';
+    blocklyDiv.style.top = y + 'px';
+    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+    Blockly.svgResize(workspace);
+  };
+  window.addEventListener('resize', onresize, false);
+  onresize();
+  Blockly.svgResize(workspace);
+workspace.addChangeListener(updateFunc);
+
+}
