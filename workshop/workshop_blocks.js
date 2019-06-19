08f3a63e2f141d2c7889333b303edde36a5e5f5a
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2019-06-19 16:10:44 -0500

    Update workshop_blocks.js

--- workshop/workshop_blocks.js
+++ workshop/workshop_blocks.js
@@ -15,7 +15,7 @@ Blockly.JavaScript['workshop_block_description'] = function(block) {
   var text_block_name = block.getFieldValue('block_name');
   var statements_init = Blockly.JavaScript.statementToCode(block, 'init');
   // TODO: Assemble JavaScript into code variable.
-  var code = "Blockly.Blocks['" + safeText(text_block_name) + "'] = {};\u001FBlockly.JavaScript['" + safeText(text_block_name) + "'] = function(block) {};";
+  var code = "Blockly.Blocks['" + safeText(text_block_name) + "'] = {\n" + statements_init + "};\u001FBlockly.JavaScript['" + safeText(text_block_name) + "'] = function(block) {};";
   return code;
 };
 
