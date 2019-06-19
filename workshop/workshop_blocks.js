10315beb92110d3250461bf8cd478a95e74dda26
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2019-06-19 16:01:25 -0500

    Update workshop_blocks.js

--- workshop/workshop_blocks.js
+++ workshop/workshop_blocks.js
@@ -11,14 +11,31 @@ Blockly.Blocks['workshop_block_description'] = {
  this.setHelpUrl("");
   }
 };
-
-
-
-
 Blockly.JavaScript['workshop_block_description'] = function(block) {
   var text_block_name = block.getFieldValue('block_name');
   var statements_init = Blockly.JavaScript.statementToCode(block, 'init');
   // TODO: Assemble JavaScript into code variable.
   var code = "Blockly.Blocks['" + safeText(text_block_name) + "'] = {};\u001FBlockly.JavaScript['" + safeText(text_block_name) + "'] = function(block) {};";
+  return code;
+};
+
+
+
+Blockly.Blocks['workspace_settooltip'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("set tooltip")
+        .appendField(new Blockly.FieldTextInput(""), "tooltip");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(210);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['workspace_settooltip'] = function(block) {
+  var text_tooltip = block.getFieldValue('tooltip');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'this.setTooltip("+quote(text_tooltip,'"')+")';
   return code;
 };
