ee06d85b7b43c6af324b40fea79c702ae405d925
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2019-06-18 19:11:55 -0500

    Update workshop_blocks.js

--- workshop/workshop_blocks.js
+++ workshop/workshop_blocks.js
@@ -1,1 +1,31 @@
+Blockly.Blocks['workshop_block_description'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("name")
+        .appendField(new Blockly.FieldTextInput("block_name"), "block_name");
+    this.appendStatementInput("init")
+        .setCheck(null)
+        .appendField("init");
+    this.setColour(120);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
 
+
+
+
+Blockly.JavaScript['workshop_block_description'] = function(block) {
+  var text_block_name = block.getFieldValue('block_name');
+  var statements_init = Blockly.JavaScript.statementToCode(block, 'init');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '
+Blockly.Blocks[' + text_block_name + '] = {
+};
+
+
+
+Blockly.JavaScript[' + text_block_name + '] = function(block) {
+};';
+  return code;
+};
