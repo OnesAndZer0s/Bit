2554da7275c7ca2f9cfd860cd5f94d0305660e3c
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2019-06-20 09:05:09 -0500

    Update workshop_blocks.js

--- workshop/workshop_blocks.js
+++ workshop/workshop_blocks.js
@@ -132,3 +132,61 @@ Blockly.JavaScript['workspace_setnextstatement'] = function(block) {
   var code = 'this.setNextStatement(' + checkbox_boolean + ', ' + text_type + ');\n';
   return code;
 };
+
+
+
+Blockly.Blocks['workshop_setcolor'] = {
+  init: function() {
+    this.appendDummyInput("line")
+        .appendField("set color")
+        .appendField(new Blockly.FieldDropdown([["hue","hue"], ["hex","hex"], ["RGB","rgb"], ["HSL","hsl"]]), "type");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(210);
+ this.setTooltip("");
+ this.setHelpUrl("");
+this.getField('type').setValidator(function(option) {
+this.sourceBlock_.updateShape_(option);
+});
+  },
+      mutationToDom: function() {
+var container = document.createElement('mutation');
+container.setAttribute('input', this.getFieldValue('type'));
+    return container;
+  },
+  domToMutation: function(xmlElement) {
+    this.updateShape_(xmlElement.getAttribute('input'));
+  },
+  updateShape_: function(input) {
+var line = this.getInput('line');
+if (input !== "hue" && this.getField('hue') !== null) {line.removeField("hue");}
+if (input !== "hex" && this.getField('hex') !== null) {line.removeField("hex");}
+if (input !== "rgb" && this.getField('r') !== null) {line.removeField("r");line.removeField("g");line.removeField("b");}
+if (input !== "hsl" && this.getField('h') !== null) {line.removeField("h");line.removeField("s");line.removeField("l");}
+
+    if (input == "hue" && this.getField('hue') == null) {
+line.appendField(new Blockly.FieldNumber(0, 0, 360), "hue");
+}
+    if (input == "hex" && this.getField('hex') == null) {
+line.appendField(new Blockly.FieldNumber(0, 0, 360), "hue");
+}   
+    if (input == "rgb" && this.getField('r') == null) {
+line.appendField(new Blockly.FieldNumber(0, 0, 255), "r");
+line.appendField(new Blockly.FieldNumber(0, 0, 255), "g");
+line.appendField(new Blockly.FieldNumber(0, 0, 255), "b");
+}   
+    if (input == "hsl" && this.getField('h') == null) {
+line.appendField(new Blockly.FieldNumber(0, 0, 360), "h");
+line.appendField(new Blockly.FieldNumber(0, 0, 100), "s");
+line.appendField(new Blockly.FieldNumber(0, 0, 100), "l");
+}   
+
+
+}
+};
+Blockly.JavaScript['workshop_setcolor'] = function(block) {
+  var dropdown_type = block.getFieldValue('type');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'this.setColour('+'VARTYPES'+')';
+  return code;
+};
