4ba38ddaf1535b062a08d9fbcc6aa9760ad785dd
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2019-06-20 11:11:25 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -39,9 +39,8 @@ function quote(text,quoteType){
 return text;
 }
 
-function toHexadecimal(inputString,type){
-if (type == "rgb") {
-inputString = inputString.map(function(cur){
+function numToRGB(rgbArr){
+return rgbArr.map(function(cur){
 if (Number(cur) !== NaN) {
 var hex = Number(cur).toString(16);
   if (hex.length < 2) {
@@ -49,12 +48,40 @@ var hex = Number(cur).toString(16);
   }
 } else {var hex = "00";}
   return hex;
-});
-console.log(inputString);
-return inputString.join('');
+}).join('');
+}
+
+function HSLtoRGB(h, s, l){
+    var r, g, b;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        var hue2rgb = function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+function toHexadecimal(inputString,type){
+if (type == "rgb") {
+return toHexadecimal(numToRGB(inputString));
 }
 else if (type == "hsl") {
-return ""
+return toHexadecimal(HSLtoRGB(inputString[0],inputString[1],inputString[2]),"rgb");
 }
 else {
 for (var i = 0, x = [], num = 0; i-num <= 5; i++) {
