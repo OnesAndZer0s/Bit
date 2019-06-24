b7c2382feb410cd86a6811a950d35eb1fd8110eb
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2019-06-24 17:46:25 -0500

    Update workshop_blocks.js

--- workshop/workshop_blocks.js
+++ workshop/workshop_blocks.js
@@ -91,10 +91,11 @@ else if (inputArr[1] == false) {
 var i = 0, arr = [];
 while (this.getInput("name"+i)) {arr.push('name'+i); i++;}
       
-console.log(arr.length);
-console.log( this.getFieldValue("functionAmnt"));
-
 if (arr.length !== Number(this.getFieldValue("functionAmnt"))) {
+var oldNum = arr.length,
+    newNum = Number(this.getFieldValue("functionAmnt"));
+console.log([oldNum, newNum]);  
+
 this.appendDummyInput("name"+i)
 .appendField(new Blockly.FieldTextInput("name"), "name"+i);
 this.appendStatementInput("funcCode"+i)
