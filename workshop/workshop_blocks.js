de57fe028422ee4c31016243622f78e6fa152f50
OnesAndZer0s <37850372+OnesAndZer0s@users.noreply.github.com>
2019-06-24 10:29:04 -0500

    Update workshop_blocks.js

--- workshop/workshop_blocks.js
+++ workshop/workshop_blocks.js
@@ -66,7 +66,8 @@ this.getInput("dtm").dispose();
 this.getInput("dtmInput").dispose();
 this.getInput("mtd").dispose();
 this.getInput("mtdInput").dispose();
-
+//this.initSvg();
+this.render();
 }
 
 }
